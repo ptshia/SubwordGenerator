4bdf16bcfec81dcc5f90a134c32910b26c3f27b7
Sakshi <imcapturingsunshine@gmail.com>
2020-11-16 00:08:35 +0530

    Initial commit for subword generator
    A console application that creates subwords from a series of alphabets and retrieves its definition from a public API

---
+++ SubwordGenerator/Infrastructure/DictionaryConnector.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+
+namespace SubwordGenerator.Infrastructure
+{
+    public class OxfordDictionaryConnector
+    {
+        public OxfordDictionaryConnector()
+        {
+
+        }
+
+        public string SearchDefinition(string word)
+        {
+            try
+            {
+                var webRequest = System.Net.WebRequest.Create("https://api.dictionaryapi.dev/api/v2/entries/en/" + word);
+                if (webRequest != null)
+                {
+                    webRequest.Method = "GET";
+                    webRequest.ContentType = "application/json";
+                    webRequest.Timeout = 10000;
+                }
+                using (System.IO.Stream s = webRequest.GetResponse().GetResponseStream())
+                {
+                    using (System.IO.StreamReader sr = new System.IO.StreamReader(s))
+                    {
+                        var responseJson = sr.ReadToEnd();
+                        dynamic response = JArray.Parse(responseJson);
+                        string definition = response.First.meanings.First.definitions.First.definition;
+                        return definition;
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                return "";
+            }
+        }
+    }
+}
---
+++ SubwordGenerator/TrieNode.cs
@@ -0,0 +1,16 @@
+﻿namespace SubwordGenerator
+{
+	public class TrieNode
+	{
+		public TrieNode[] Child = new TrieNode[26];
+		public bool leaf;
+		public TrieNode()
+		{
+			leaf = false;
+			for (int i = 0; i < 26; i++)
+			{
+				Child[i] = null;
+			}
+		}
+	}
+}
